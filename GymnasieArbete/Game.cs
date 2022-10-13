78e781a071bdfe4cb2bb6c3f094aebe430b88fe3
Glitch <caleb.samuel.brown@gmail.com>
2022-10-13 10:15:14 +0200

    first commit

---
+++ GymnasieArbete/Game.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Media;
+using Raylib_cs;
+
+
+public class Game
+{
+    public static void StartGame()
+    {
+        
+    }
+}
---
+++ GymnasieArbete/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Media;
+using Raylib_cs;
+
+bool main = true, startGame = false;
+
+if(main == true)
+{
+    startGame = MainMenu();
+    main = false;
+}
+if(startGame == true)
+{
+    Game.StartGame();
+}
+
+static bool MainMenu()
+{
+    return true;
+}
